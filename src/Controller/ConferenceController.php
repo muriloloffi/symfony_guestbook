2e4cf1ec56d60bc68644136adf8577a7310f4011
Murilo <19322082+muriloloffi@users.noreply.github.com>
2022-07-19 22:41:03 -0300

    [Feat] Add conferences nav menu

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -26,12 +26,13 @@ class ConferenceController extends AbstractController
     }
 
     #[Route('/conference/{id}', name: 'conference')]
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, ConferenceRepository $conferenceRepository): Response
     {
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
         return new Response($this->twig->render('conference/show.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
             'conference' => $conference,
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
